0cb1593718e8da4de2256af975be2b43446a9765
Jessie Beech <jessie@jessiebeech.com>
2018-12-17 11:49:59 +0000

    basic file upload component Relates #11

---
+++ src/components/upload.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react"
+import axios from "axios"
+import fileNameFormatter from "../utils/fileNameFormat"
+
+const endpoint = "http://localhost:5000/upload"
+
+class Upload extends Component {
+  state = {
+    file: null,
+  }
+  onImageSelect = () => {
+    this.setState({
+      file: event.target.files[0],
+    })
+  }
+  sendImage = () => {
+    const data = new FormData()
+    const { file } = this.state
+    const fileName = fileNameFormatter(file.name)
+    data.set("file", file, fileName)
+    axios.post(endpoint, data).then(res => {
+      console.log(res.statusText)
+    })
+  }
+  render() {
+    const { file } = this.state
+    return (
+      <form>
+        <label htmlFor="image">Click to upload:</label>
+        <input
+          type="file"
+          id="image"
+          name="image"
+          accept="image/png, image/jpeg"
+          multiple={false}
+          files={file}
+          onChange={this.onImageSelect}
+        />
+        <button onClick={this.sendImage}>Send</button>
+      </form>
+    )
+  }
+}
+
+export default Upload
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
-import React from 'react'
-import Layout from '../components/layout'
-import SEO from '../components/seo'
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Upload from "../components/upload"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
-    <h1>Hi people</h1>
-    <p>This is the index page.</p>{' '}
+    <SEO title="Home" keywords={["gatsby", "application", "react"]} />
+    <h1>Index Page</h1>
+    <Upload />
   </Layout>
 )
 
