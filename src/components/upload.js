fc997c99723b3a853d2facf2a358290e19652b37
Jessie Beech <jessie@jessiebeech.com>
2018-12-18 12:25:57 +0000

    added validtion and tests:

--- src/components/upload.js
+++ src/components/upload.js
@@ -2,6 +2,7 @@ import React, { Component } from "react"
 import axios from "axios"
 import fileNameFormatter from "../utils/fileNameFormat"
 import styled from "styled-components"
+import isValidFileType from "../utils/isValidFileType"
 
 const endpoint = "http://localhost:5000/upload"
 
@@ -28,40 +29,75 @@ const Clickable = styled.div.attrs({
 
 const Label = styled.label.attrs({
   className: "apercu h-100 w-100 flex items-center justify-center",
-})``
+})`
+  p {
+    width: 50%;
+  }
+`
 
 class Upload extends Component {
   state = {
     file: null,
     fileName: null,
+    error: "",
+  }
+  validateImage = file => {
+    if (!isValidFileType(file)) {
+      this.setState({
+        error:
+          "Oops, it looks like your file is the wrong type! Try uploading a jpg or png.",
+      })
+      return false
+    }
+    if (file.size > 1048576) {
+      this.setState({
+        error:
+          "Oops, it looks like your file is to big! Try uploading a smaller image.",
+      })
+      return false
+    }
+    this.setState({ error: "" })
+    return true
   }
   onImageSelect = event => {
-    this.setState({
-      file: event.target.files[0],
-      fileName: fileNameFormatter(event.target.files[0].name),
-    })
-    // this.sendImage()
+    const file = event.target.files[0]
+    if (this.validateImage(file)) {
+      this.setState({
+        file,
+        fileName: fileNameFormatter(file.name),
+      })
+    }
   }
   sendImage = () => {
     const data = new FormData()
     const { file, fileName } = this.state
     data.set("file", file, fileName)
-    axios.post(endpoint, data).then(res => {
-      console.log(res.statusText)
-      this.setState({
-        file: null,
-        fileName: null,
+    axios
+      .post(endpoint, data)
+      .then(res => {
+        console.log(res.statusText)
+        this.setState({
+          file: null,
+          fileName: null,
+        })
       })
-    })
+      .catch(err => {
+        console.log(err)
+      })
   }
   render() {
-    const { file, fileName } = this.state
+    const { file, error } = this.state
     return (
       <form>
         <Clickable>
           <Label htmlFor="image">
-            <p>Click to upload</p>
-            {fileName && <p>:{fileName}</p>}
+            {error ? (
+              <p>{error}</p>
+            ) : file ? (
+              <p>Loading...</p>
+            ) : (
+              <p>Click to upload</p>
+            )}
           </Label>
           <FileInput
             type="file"
