145d36ef746cc230032d778f8eab6296ce2210f0
martingaston <squidman@gmail.com>
2018-12-19 12:27:19 +0000

    plumb image_watermark into api call Relates #31

--- api/meme_generator/image_watermark.py
+++ api/meme_generator/image_watermark.py
@@ -1,22 +1,26 @@
 from PIL import Image
+from pathlib import Path
 
 
-def add_watermark(input_path, output="watermarked"):
+def add_watermark(input_path, watermark_path, output_path):
     """Add EU-compliant meme watermark to a supplied image"""
+    print(input_path)
+    print(watermark_path)
+    print(output_path)
     try:
         original_image = Image.open(input_path)
         _original_width, original_height = original_image.size
 
-        watermark_image = Image.open("eu-compliant-watermark.png")
+        watermark_image = Image.open(watermark_path)
         watermark_width, watermark_height = watermark_image.size
         watermark_resized = watermark_image.resize(
-            (watermark_width/2, watermark_height/2))
+            (115, 115))
         _w_width, w_height = watermark_resized.size
 
         original_image.paste(watermark_resized, (8, original_height - w_height - 8),
                              mask=watermark_resized)
 
-        original_image.save(output + ".jpg")
+        original_image.save(output_path)
         return "Success!"
     except IOError:
         return "Failure!"
