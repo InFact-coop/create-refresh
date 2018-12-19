36a228361bf06c1e0006a7675af92a9df19a0525
Jessie Beech <jessie@jessiebeech.com>
2018-12-19 16:08:39 +0000

    added styled form skeleton Relates #12

---
+++ src/components/signup.js
@@ -0,0 +1,79 @@
+import React from "react"
+import styled from "styled-components"
+import bitSteps from "../assets/images/8bit.svg"
+
+const Section = styled.section.attrs({
+  className: "flex justify-center pv4 w-100",
+})``
+
+const Container = styled.div.attrs({
+  className:
+    "flex pa4-ns pa3 justify-between flex-column flex-row-ns flex-column-reverse ",
+})`
+  background: url(${bitSteps}), var(--pink);
+  background-position: bottom right;
+  background-repeat: no-repeat;
+  max-width: 797px;
+`
+
+const Input = styled.input.attrs({
+  className:
+    "b--dark-pink ba dark-pink bg-pink mv1 db pl2 w5-ns w-100 apercu font-5",
+})`
+  border-width: 3px;
+  &::placeholder {
+    color: var(--dark-pink);
+  }
+`
+
+const Checkbox = styled.div.attrs({
+  className: "b--dark-pink bw1 ba pink mr2",
+})`
+  border-width: 3px;
+  min-width: 16px;
+  min-height: 16px;
+`
+const FormButton = styled.button.attrs({
+  className: "bg-dark-pink light-pink bn ph2 tc apercu font-5 ma0-ns mv3",
+})`
+  width: 184px;
+  height: 40px;
+`
+
+const InfoText = styled.div.attrs({
+  className:
+    "w-30-ns w-100 pa0-ns pv3 tr-ns tl dark-pink font-5-ns mobile-body",
+})``
+
+const Signup = () => (
+  <Section>
+    <Container>
+      <form>
+        <Input name="firstname" placeholder="First Name" type="text" required />
+        <Input name="lastname" placeholder="Last Name" type="text" required />
+        <Input name="email" placeholder="Email" type="email" required />
+        <Input name="country" placeholder="Country" type="text" required />
+        <Input
+          name="socialmedia"
+          placeholder="@SocialMediaHandle"
+          type="text"
+        />
+        <div className="flex items-center w5-ns w-100 mv2">
+          <Checkbox checked={false} />
+          <p className="font-7 dark-pink">
+            I allow Create.Refresh to use my contact information to send me
+            email updates about campaign activities.*
+          </p>
+        </div>
+        <FormButton type="button">Sign Up</FormButton>
+      </form>
+      <InfoText>
+        We’re building a network of people dedicated to fixing copyright,
+        protecting memes, and saving our Internet forever. Will you join our
+        network and help stop Article 13?
+      </InfoText>
+    </Container>
+  </Section>
+)
+
+export default Signup
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,14 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Upload from "../components/upload"
+import Signup from "../components/signup"
+
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={["gatsby", "application", "react"]} />
     <Upload />
+    <Signup />
   </Layout>
 )
 
