aea35095d966a3d47084d980e4defa843085a10a
squidmania <squidman@gmail.com>
2018-12-20 15:14:45 +0000

    add info component with subheading/description Relates #10

---
+++ src/components/info.js
@@ -0,0 +1,35 @@
+import React from "react"
+import styled from "styled-components"
+
+const Container = styled.div.attrs({
+  className:
+    "mid-gray flex flex-column flex-row-ns items-center justify-center mt2 mt4-ns mb4-ns",
+})``
+
+const Column = ({ className, children }) => (
+  <div
+    className={className.concat(
+      " ma3-ns pa1 pa4-ns lh-copy h5 flex w-30-ns items-center justify-center tc"
+    )}
+  >
+    {children}
+  </div>
+)
+
+const Strap = styled(Column).attrs({ className: "ttu f4 f3-ns" })``
+
+const Description = styled(Column).attrs({ className: "" })``
+
+const Info = () => (
+  <Container>
+    <Strap>The EU compliant meme generator is here to save your memes</Strap>
+    <Description>
+      Article 13 is putting your memes at risk. But the EU Compliant Meme
+      Generator is here to save them from the purge. Simply upload your meme (or
+      picture of your cat—both are acceptable) and watch as your picture becomes
+      Certified EU Compliant™. Try for yourself!
+    </Description>
+  </Container>
+)
+
+export default Info
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,15 +3,17 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Upload from "../components/upload"
+import Info from "../components/info"
+import Video from "../components/video"
 import Signup from "../components/signup"
 import Faq from "../components/faq"
-import Video from "../components/video"
 import Footer from "../components/footer"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={["gatsby", "application", "react"]} />
     <Upload />
+    <Info />
     <Video />
     <Signup />
     <Faq />
