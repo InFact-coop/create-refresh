87c312ae0ddb461ce6a5233ac590ea46a7a53f2a
Jessie Beech <jessie@jessiebeech.com>
2019-01-04 17:26:21 +0000

    refactor for large screen

--- src/components/video.js
+++ src/components/video.js
@@ -9,23 +9,19 @@ const VideoContainer = styled.div.attrs({
   className: "flex justify-center align-center bg-black mb4 mb0-ns",
 })`
   width: 80vw;
+  max-width: 635px;
 `
 
-const IFrame = styled.iframe.attrs({
-  className: "",
-})`
-  max-width: 70vw;
-  max-height: 70vw;
+const IFrame = styled.iframe`
+  @media (min-height: 40em) {
+    max-width: 70vw;
+    max-height: 70vw;
+  }
 `
-
-const LetterBox = styled.div.attrs({
-  className: "w4-ns db-ns dn",
-})``
 
 const Video = () => (
   <Background>
     <VideoContainer>
-      <LetterBox />
       <IFrame
         src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreate.refresh%2Fvideos%2F2276532395966735%2F&show_text=0&width=476"
         width="436"
@@ -38,7 +34,6 @@ const Video = () => (
         frameBorder="0"
         allowFullScreen={true}
       />
-      <LetterBox />
     </VideoContainer>
   </Background>
 )
