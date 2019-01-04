58a1b7b6bdf60f9e40675c084c543d2fd6abb768
Jessie Beech <jessie@jessiebeech.com>
2019-01-04 17:19:28 +0000

    size iframe on mobile

--- src/components/video.js
+++ src/components/video.js
@@ -6,20 +6,27 @@ const Background = styled.div.attrs({
 })``
 
 const VideoContainer = styled.div.attrs({
-  className: "flex justify-center bg-black align-center",
+  className: "flex justify-center align-center bg-black mb4 mb0-ns",
 })`
-  max-width: 100vw;
+  width: 80vw;
+`
+
+const IFrame = styled.iframe.attrs({
+  className: "",
+})`
+  max-width: 70vw;
+  max-height: 70vw;
 `
 
 const LetterBox = styled.div.attrs({
-  className: "w4-ns",
+  className: "w4-ns db-ns dn",
 })``
 
 const Video = () => (
   <Background>
     <VideoContainer>
       <LetterBox />
-      <iframe
+      <IFrame
         src="https://www.facebook.com/plugins/video.php?href=https%3A%2F%2Fwww.facebook.com%2Fcreate.refresh%2Fvideos%2F2276532395966735%2F&show_text=0&width=476"
         width="436"
         height="436"
