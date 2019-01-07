5eb2bedd81b58d2a03f26945393a68a26ca18049
martingaston <squidman@gmail.com>
2019-01-07 22:48:13 +0000

    add google tag manger script relates #26

--- src/utils/trackingScripts.js
+++ src/utils/trackingScripts.js
@@ -12,6 +12,17 @@ const googleScript = () => {
   document.head.appendChild(newScript)
 }
 
+const googleTagManager = () => {
+  const newScript = document.createElement("script")
+  newScript.type = "text/javascript"
+  newScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-PHXNVJF');`
+  document.head.appendChild(newScript)
+}
+
 const facebookScript = () => {
   const newScript = document.createElement("script")
   newScript.type = "text/javascript"
@@ -23,6 +34,7 @@ const facebookScript = () => {
 const appendTrackingScripts = () => {
   addGoogleLink()
   googleScript()
+  googleTagManager()
   facebookScript()
 }
 
