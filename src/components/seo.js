f6b45c7a0c2357490409c1f080c919b3bf22bb94
lucymk <lucy.m.king92@gmail.com>
2019-01-10 12:07:19 +0000

    add twittter script again

--- src/components/seo.js
+++ src/components/seo.js
@@ -75,23 +75,17 @@ function SEO({ lang, title, image }) {
       ]}
     >
       <script>{`
-            <script>window.twttr = (function(d, s, id) {
-              var js, fjs = d.getElementsByTagName(s)[0],
-                t = window.twttr || {};
-              if (d.getElementById(id)) return t;
-              js = d.createElement(s);
-              js.id = id;
-              js.src = "https://platform.twitter.com/widgets.js";
-              fjs.parentNode.insertBefore(js, fjs);
-            
-              t._e = [];
-              t.ready = function(f) {
-                t._e.push(f);
-              };
-            
-              return t;
-            }(document, "script", "twitter-wjs"));</script>
-            
+          window.twttr = (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0],t = window.twttr || {};
+            if (d.getElementById(id)) return t;
+            js = d.createElement(s);js.id = id;
+            js.src = 'https://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js, fjs);
+            t._e = [];
+            t.ready = function(f) {t._e.push(f);
+            }; 
+          return t;
+        }(document, 'script', 'twitter-wjs'));
             `}</script>
       <script>
         {`
