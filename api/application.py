c23cf62ae69c2526ceaa49e57f615c17deef99f8
martingaston <squidman@gmail.com>
2019-01-10 10:31:15 +0000

    remove debug mode from aws application.py, relates #32

--- api/application.py
+++ api/application.py
@@ -4,5 +4,4 @@ from meme_generator import create_app
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     application = create_app()
-    application.debug = True
     application.run()
