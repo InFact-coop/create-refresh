3dc3b4341e6afa4ec6568cd6e8482d9db0a7c988
lucymk <lucy.m.king92@gmail.com>
2019-01-11 23:57:56 +0000

    adds nextjs files and config, updates package.json

---
+++ next.config.js
@@ -0,0 +1,52 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+
+module.exports = {
+  webpack(config) {
+    config.module.rules.push(
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // you can specify a publicPath here
+              // by default it use publicPath in webpackOptions.output
+              publicPath: "../",
+            },
+          },
+          "css-loader",
+          "postcss-loader",
+        ],
+      },
+      {
+        test: /\.(otf|ttf)$/,
+        use: {
+          loader: "url-loader",
+          options: {
+            name: "static/fonts/[name].[ext]",
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|gif|svg|jpeg|)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "static/images/[name].[ext]",
+          },
+        },
+      }
+    )
+
+    config.plugins.push(
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css",
+      })
+    )
+
+    return config
+  },
+}
