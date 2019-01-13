5195946400ecb8b5d8d3b87008610d2449d9be53
lucymk <lucy.m.king92@gmail.com>
2019-01-13 19:41:14 +0000

    adds separate cartoon page

---
+++ pages/cartoon.js
@@ -0,0 +1,112 @@
+import React, { Component } from "react"
+import Router from "next/router"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Upload from "../components/upload"
+import Info from "../components/info"
+import Video from "../components/video"
+import Signup from "../components/signup"
+import Faq from "../components/faq"
+import Footer from "../components/footer"
+
+import encode from "../utils/encode"
+
+import "../styles/index.css"
+
+class CartoonPage extends Component {
+  state = {
+    fileURL: null,
+    error: "",
+    cartoon: null,
+    view: "cartoon",
+    showMenu: false,
+    showShareModal: false,
+    formCompleted: false,
+  }
+
+  static async getInitialProps({ query }) {
+    const { cartoon, formCompleted } = query
+
+    // do api call to get cartoon and original image using id from aws
+    // if no original redirect to home
+
+    return {
+      cartoon: "https://tinyurl.com/ybhexc65",
+      fileURL: "https://tinyurl.com/ybhexc65",
+      formCompleted,
+    }
+  }
+
+  submitForm = data => {
+    this.setState(
+      {
+        formCompleted: true,
+      },
+      this.postData(data)
+    )
+  }
+
+  postData = data => {
+    // post user information to proxy Mailchimp server
+    const URIdata = encode({ ...data })
+    fetch("http://127.0.0.1:5000/subscribe", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: URIdata,
+    })
+      .then(response => this.setState({ submitted: response.json() }))
+      .catch(error => {
+        //eslint-disable-next-line
+        console.log(error)
+        this.setState({ error: true })
+      })
+  }
+
+  toggleMenu = () => {
+    this.setState(prevProps => ({ showMenu: !prevProps.showMenu }))
+  }
+
+  toggleShareModal = () => {
+    this.setState(prevProps => ({ showShareModal: !prevProps.showShareModal }))
+  }
+
+  handleStartOver = () => {
+    Router.push({
+      pathname: "/",
+    })
+  }
+
+  render() {
+    const { error, view, showMenu, showShareModal } = this.state
+
+    const { fileURL, cartoon } = this.props
+    return (
+      <Layout>
+        <SEO
+          title="Make ANY Meme EU Complaint!"
+          description="Try the EU Compliant Meme Generator for yourself"
+          image={cartoon}
+        />
+        <Upload
+          fileURL={fileURL}
+          error={error}
+          cartoon={cartoon}
+          view={view}
+          showMenu={showMenu}
+          showShareModal={showShareModal}
+          handleStartOver={this.handleStartOver}
+          toggleShareModal={this.toggleShareModal}
+          toggleMenu={this.toggleMenu}
+        />
+        <Info />
+        <Video />
+        <Signup theme="light" submitForm={this.submitForm} />
+        <Faq />
+        <Footer />
+      </Layout>
+    )
+  }
+}
+
+export default CartoonPage
