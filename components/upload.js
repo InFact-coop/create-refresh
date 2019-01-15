7f712e93ac348bf45858cfb9d6f6561a5ce18060
martingaston <squidman@gmail.com>
2019-01-15 16:51:11 +0000

    add styling for mobile/tablet view, relates #107

--- components/upload.js
+++ components/upload.js
@@ -17,7 +17,7 @@ import SignUp from "./signup"
 
 const ImagesSidebyside = styled.div.attrs({
   className:
-    "data-view flex flex-column flex-row-ns justify-center items-center mv3 mv0-ns",
+    "data-view w-100 flex flex-column flex-row-ns justify-center items-center mv3 mv0-ns",
 })``
 
 const Image = styled.div.attrs({ className: "ma2 image-comparison" })`
