2b9161c6980c19e864d2b4876b3bb76624aa128e
lucymk <lucy.m.king92@gmail.com>
2019-01-16 10:56:38 +0000

    update metadata file type

--- components/seo.js
+++ components/seo.js
@@ -3,7 +3,7 @@ import Head from "next/head"
 function SEO({ title, description, image, cartoonId }) {
   const defaultDescription = "Try the EU Compliant Meme Generator for yourself"
   const defaultTitle = "Make ANY Meme EU Compliant!"
-  const defaultImage = "https://ibb.co/kXjc3zR"
+  const defaultImage = "https://i.ibb.co/VTRBHb7/metadata.png"
   return (
     <Head>
       <meta charSet="UTF-8" />
