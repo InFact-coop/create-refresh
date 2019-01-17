79466c00b4c3c1fb1239009e097b94e76d8efcba
maxgerber <mgerber94@gmail.com>
2019-01-17 11:06:58 +0000

    Second attempt, with simple redirecting middleware
    Relates #109

--- server.js
+++ server.js
@@ -4,24 +4,26 @@
 const { createServer } = require("http")
 const { parse, format } = require("url")
 const next = require("next")
+const express = require("express")
 
 const dev = process.env.NODE_ENV !== "production"
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const nextApp = next({ dev })
+const handle = nextApp.getRequestHandler()
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    // Be sure to pass `true` as the second argument to `url.parse`.
-    // This tells it to parse the query portion of the URL.
-    const parsedUrl = parse(req.url, true)
+nextApp.prepare().then(() => {
+  const app = express()
 
-    const secureUrl = {
-      ...parsedUrl,
-      protocol: "https:",
+  app.use((req, res, next) => {
+    if (!req.secure) {
+      return res.redirect(["https://", req.get("Host"), req.url].join(""))
     }
-    handle(req, res, secureUrl)
-  }).listen(3000, err => {
+    next()
+  })
+
+  app.get("*", (req, res) => handle(req, res))
+
+  app.listen(3000, err => {
     if (err) throw err
-    console.log("> Ready on http://localhost:3000")
+    console.log("Ready on http://localhost:3000")
   })
 })
