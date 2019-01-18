ff9383da0f93f458836569406eba5c759098d2a3
lucymk <lucy.m.king92@gmail.com>
2019-01-18 19:11:16 +0000

    redirecting www

--- server.js
+++ server.js
@@ -9,12 +9,26 @@ const handle = nextApp.getRequestHandler()
 nextApp.prepare().then(() => {
   const app = express()
 
-  // app.use((req, res, next) => {
-  //   if (req.get("X-Forwarded-Proto") !== "https" && !dev) {
-  //     return res.redirect(["https://", req.get("Host"), req.url].join(""))
-  //   }
-  //   next()
-  // })
+  app.use(
+    // (req, res, next) => {
+    //   if (req.get("X-Forwarded-Proto") !== "https" && !dev) {
+    //     return res.redirect(["https://", req.get("Host"), req.url].join(""))
+    //   }
+    //   next()
+    // },
+    (req, res, next) => {
+      if (req.headers.host.slice(0, 4) === "www.") {
+        const newHost = req.headers.host.slice(4)
+        return res.redirect(
+          301,
+          req.protocol + "://" + newHost + req.originalUrl
+        )
+      }
+      next()
+    }
+  )
+
+  app.set("trust proxy", true)
 
   app.get("*", (req, res) => handle(req, res))
 
