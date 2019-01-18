ff09f5f5aaa311b13b5027b8b996d3dcf9199e81
lucymk <lucy.m.king92@gmail.com>
2019-01-18 19:16:23 +0000

    rest servr

--- server.js
+++ server.js
@@ -9,12 +9,28 @@ const handle = nextApp.getRequestHandler()
 nextApp.prepare().then(() => {
   const app = express()
 
-  // app.use((req, res, next) => {
+  // probably something like the second for redirecting www -> root but doesn't work yet
+
+  // app.use(
+  // (req, res, next) => {
   //   if (req.get("X-Forwarded-Proto") !== "https" && !dev) {
   //     return res.redirect(["https://", req.get("Host"), req.url].join(""))
   //   }
   //   next()
-  // })
+  // },
+  //   (req, res, next) => {
+  //     if (req.headers.host.slice(0, 4) === "www.") {
+  //       const newHost = req.headers.host.slice(4)
+  //       return res.redirect(
+  //         301,
+  //         req.protocol + "://" + newHost + req.originalUrl
+  //       )
+  //     }
+  //     next()
+  //   }
+  // )
+
+  // app.set("trust proxy", true)
 
   app.get("*", (req, res) => handle(req, res))
 
