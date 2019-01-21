f58e6bdac3c402b548215fddbd18d29613751f7b
lucymk <lucy.m.king92@gmail.com>
2019-01-21 17:52:08 +0000

    removes server

--- server.js
+++ server.js
@@ -1,42 +1,25 @@
-const nextJs = require("next")
-const express = require("express")
+// const nextJs = require("next")
+// const express = require("express")
 
-const dev = process.env.NODE_ENV !== "production"
+// const dev = process.env.NODE_ENV !== "production"
 
-const nextApp = nextJs({ dev })
-const handle = nextApp.getRequestHandler()
+// const nextApp = nextJs({ dev })
+// const handle = nextApp.getRequestHandler()
 
-nextApp.prepare().then(() => {
-  const app = express()
+// nextApp.prepare().then(() => {
+//   const app = express()
 
-  app.use((req, res, next) => {
-    if (req.get("X-Forwarded-Proto") !== "https" && !dev) {
-      return res.redirect(["https://", req.get("Host"), req.url].join(""))
-    }
-    next()
-  })
+//   app.use((req, res, next) => {
+//     if (req.get("X-Forwarded-Proto") !== "https" && !dev) {
+//       return res.redirect(["https://", req.get("Host"), req.url].join(""))
+//     }
+//     next()
+//   })
 
-  app.get("*", (req, res) => handle(req, res))
+//   app.get("*", (req, res) => handle(req, res))
 
-  app.listen(process.env.PORT || 3000, err => {
-    if (err) throw err
-    console.log("💃 Ready on http://localhost:3000 🕺")
-  })
-
-  // probably something like the second for redirecting www -> root but doesn't work yet
-
-  //   (req, res, next) => {
-  //     if (req.headers.host.slice(0, 4) === "www.") {
-  //       const newHost = req.headers.host.slice(4)
-  //       return res.redirect(
-  //         301,
-  //         req.protocol + "://" + newHost + req.originalUrl
-  //       )
-  //     }
-  //     next()
-  //   }
-  // )
-
-  // app.set("trust proxy", true)
-  
-})
+//   app.listen(process.env.PORT || 3000, err => {
+//     if (err) throw err
+//     console.log("💃 Ready on http://localhost:3000 🕺")
+//   })
+// })
